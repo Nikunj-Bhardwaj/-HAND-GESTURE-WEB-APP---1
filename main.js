d72dcec28aba01ca1f8d0c343d37c97eb8adf3bc
Nikunj-Bhardwaj <88379545+Nikunj-Bhardwaj@users.noreply.github.com>
2021-12-23 16:17:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-prediction_1 =""
-prediction_2=""
 
+Webcam.attach( '#camera' );
 
-
-Webcam.set({
+camera = document.getElementById("camera");
+     
+  Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
+  });
 
-});
-
-camera = document.getElementById("camera");
-
-Webcam.attach( '#camera');
-
-
-function take_snapshot(){
-    Webcam.snap(function (data_url){
-        document.getElementById("result").innerHTML='<img id="captured_image"src="'+data_url+'"/>';
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
-console.log('ml5 version',ml5.version);
-classifier = ml5.imageClassifier('https://unpkg.com/ml5@0.4.3/dist/ml5.min.js', modelLoaded);
-function modelLoaded(){
-console.log('modelLoaded');
-}
+  console.log('ml5 version:', ml5.version);
+ 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EdLqqwv33/',modelLoaded);
+
+ 
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+     
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
 
 
-function speak(){
-var synth= window.speechSynthesis;
-speak_data_1 = "the first prediction is "+prediction_1;
-speak_data_2 = "the second prediction is "+prediction_2;
-var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
-synth.speak(utterThis);
 
-}
+function gotResult(error, results) {
+
+  if (error) {
+    console.error(error);
+  } else {
+   
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;}}
